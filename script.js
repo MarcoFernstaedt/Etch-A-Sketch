62840352698e8390cf443794cfbe2bc624159247
Marco <fernstaedtmarco@gmail.com>
2022-07-20 16:13:34 -0700

    adding comments to explaine code

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// grabbing container div
 const container = document.getElementById("container")
+// adding css rules to container div
 container.style.cssText = "display: grid; grid-template-columns: repeat(16, 2fr); grid-template-rows: repeat(16, 2fr); gap: 0; width: 100%; height: 100%;"
 
+// bullds div's needed for 16 x 16 grid layout
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div")
     div.classList.add("canvas")
