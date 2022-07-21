f8e9b5042be3ed85de28e374ce834a56b692ba45
Marco <fernstaedtmarco@gmail.com>
2022-07-20 18:12:31 -0700

    edited javascript file ability to change color of div when hover or clicked

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // grabbing container div
 const container = document.getElementById("container")
+
 // adding css rules to container div
-container.style.cssText = "display: grid; grid-template-columns: repeat(16, 2fr); grid-template-rows: repeat(16, 2fr); gap: 0; width: 100%; height: 100%;"
+container.style.cssText = "height: 100%; display: grid; grid-template-columns: repeat(16, 15px); grid-template-rows: repeat(16, 15px); gap: none; justify-content: center; align-items: flex-end; border: 20px solid black;"
 
 // bullds div's needed for 16 x 16 grid layout
 for (let i = 0; i < 256; i++) {
+    // creates div 
     const div = document.createElement("div")
-    div.classList.add("canvas")
-    div.textContent = "hi";
-    // div.canvas.style.cssText = "height: 100%; width: 100%; background-color:hover: white;"
+    div.style.cssText = "width: 20px; height: 20px; border: .5px solid black; border-radius: 180px;"
+    // adding event lister to add a class in order to change background-color
+    div.addEventListener("mouseenter", e => div.classList.add("hover"))
+    div.addEventListener("click", e => div.classList.remove("hover"))
+
+    // adding div to container
     container.appendChild(div)
 }
-
